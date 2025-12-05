3f22fde1807eea7076071454db78886d689c2233
ranariaz94-pixel <rana.riaz94@gmail.com>
2025-12-05 19:57:44 +0500

    a little debugging was left behind

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let wantedTransfer = document.querySelector("#wantedTransfer").value
-const btn = document.querySelector("#convert");
-const result = document.querySelector("#result");
-let transferrer = document.querySelector("#transferrer");
+// let wantedTransfer = document.querySelector("#wantedTransfer").value
+// const btn = document.querySelector("#convert");
+// const result = document.querySelector("#result");
+// let transferrer = document.querySelector("#transferrer");
 
 let input = document.querySelector("#thevaltobeconverted").value;
 
-console.log(wantedTransfer);
+
 async function currencyCoversion(){
     let response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json');
     let data = await response.json();
@@ -15,9 +15,9 @@ async function currencyCoversion(){
   result.textContent = converted.toFixed(2)
 }
 btn.addEventListener("click",() => {
-    wantedTransfer = document.querySelector("#wantedTransfer").value
-        transferrer = document.querySelector("#transferrer").value
-         input = document.querySelector("#thevaltobeconverted").value;
+   let wantedTransfer = document.querySelector("#wantedTransfer").value
+     let   transferrer = document.querySelector("#transferrer").value
+       let  input = document.querySelector("#thevaltobeconverted").value;
       
 currencyCoversion();
 })
