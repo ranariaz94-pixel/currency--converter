528d836e32589a7d76e7f4757717c8b654720c4a
ranariaz94-pixel <rana.riaz94@gmail.com>
2025-12-05 20:04:58 +0500

    error handling

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-// let wantedTransfer = document.querySelector("#wantedTransfer").value
-// const btn = document.querySelector("#convert");
-// const result = document.querySelector("#result");
-// let transferrer = document.querySelector("#transferrer");
+  let wantedTransfer = document.querySelector("#wantedTransfer").value
+ const btn = document.querySelector("#convert");
+ const result = document.querySelector("#result");
+let transferrer = document.querySelector("#transferrer");
 
 let input = document.querySelector("#thevaltobeconverted").value;
 
 
 async function currencyCoversion(){
-    let response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json');
+  try {
+    result.innerText = "loading"
+      let response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json');
     let data = await response.json();
       const amountInEUR = input / data.eur[wantedTransfer.toLowerCase()];
   const converted = amountInEUR * data.eur[transferrer.toLowerCase()];
 
   result.textContent = converted.toFixed(2)
-}
+  } catch (error) {
+    if(result.innerText === Number(result.innertext)){
+  result.innerText = "Invalid value!"
+  return;  
+  }
+  result.innerText = "an error ocurred"
+}}
 btn.addEventListener("click",() => {
-   let wantedTransfer = document.querySelector("#wantedTransfer").value
-     let   transferrer = document.querySelector("#transferrer").value
-       let  input = document.querySelector("#thevaltobeconverted").value;
       
 currencyCoversion();
 })
